a40b50ed45e3b86415235fa51a313995b401a69a
XDZC00LGUY <161362717+xdzc00lguy@users.noreply.github.com>
2026-09-12 19:32:18 -0300

    tabela

--- projeto/kali/script.lua
+++ projeto/kali/script.lua
@@ -117,9 +117,6 @@ local function detectPlayers()
             table.insert(teleportUsuarios, tostring(v.DisplayName))
         end
     end
-    if TeleportDropdown then
-        TeleportDropdown:Refresh(teleportUsuarios)
-    end
 end
 detectPlayers()
 
@@ -194,6 +191,7 @@ local TeleportButton = MainTab:CreateButton({
    Name = "Atualizar lista de usuários",
    Callback = function()
     detectPlayers()
+    TeleportDropdown:Refresh(teleportUsuarios)
     Rayfield:Notify({
         Title = "Teleport",
         Content = string.format("A lista de usuários foi atualizada com sucesso!"),
