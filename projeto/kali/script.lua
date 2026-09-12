4ccd933af9e85a3bde0e6af897ba77a46f313614
XDZC00LGUY <161362717+xdzc00lguy@users.noreply.github.com>
2026-09-12 17:42:41 -0300

    test

--- projeto/kali/script.lua
+++ projeto/kali/script.lua
@@ -218,6 +218,7 @@ local TypeTpDropdown = ConfigTab:CreateDropdown({
    MultipleOptions = false,
    Flag = "ConfigTp", -- A flag is the identifier for the configuration file; make sure every element has a different flag if you're using configuration saving to ensure no overlaps
    Callback = function(Options)
+    print(Options[1])
     TeleportType = Options[1]
    end,
 })
